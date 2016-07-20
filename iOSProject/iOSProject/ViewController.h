d8415f32a5e80e351df78a1f7689be0df9a07af2
Bondar Pavel <bond.p85@gmail.com>
2016-07-20 10:56:37 +0300

    iOSProject -> initial commit

---
+++ iOSProject/iOSProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSProject
+//
+//  Created by Bondar Pavel on 7/20/16.
+//  Copyright © 2016 Pavel Bondar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
